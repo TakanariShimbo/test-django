266e50640fbb0e0ba65360abfae22cf164dd5e36
TakanariShimbo <takanari.shimbo@gmail.com>
2024-07-12 20:43:17 +0900

    update

--- myproject/settings.py
+++ myproject/settings.py
@@ -55,7 +55,7 @@ TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [
-            BASE_DIR / "templates",
+            BASE_DIR / "myapp/templates",
         ],
         "APP_DIRS": True,
         "OPTIONS": {
